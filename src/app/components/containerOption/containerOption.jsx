53ec921d77cd531f718ef87f953b06ad304b7de3
Rickazuo <rick3209@gmail.com>
2023-06-06 19:38:19 -0300

    update style of dropright

--- src/app/components/containerOption/containerOption.jsx
+++ src/app/components/containerOption/containerOption.jsx
@@ -3,41 +3,46 @@ import styles from "./containerOption.module.css";
 import { useState } from "react";
 
 export default function ContainerOption({ title, rows }) {
-    const [hoveredIndex, setHoveredIndex] = useState(-1);
-    const [actualIndex, setActualIndex] = useState(-1);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [actualIndex, setActualIndex] = useState(-1);
 
-    const handleMouseEnter = (index) => {
-        setHoveredIndex(index);
-    };
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
 
-    const handleMouseLeave = () => {
-        setHoveredIndex(-1);
-    };
+  const handleMouseLeave = () => {
+    setHoveredIndex(-1);
+  };
 
-    console.log(hoveredIndex);
-    return (
-        <main className={styles.main}>
-            <span className={styles.title}>{title}</span>
-            <div className={styles.container}>
-                {rows.map((row, index) => (
-                    <div key={index}>
-                        <div
-                            onMouseEnter={() => handleMouseEnter(index)}
-                            onMouseLeave={handleMouseLeave}
-                            onClick={() => setActualIndex(index)}
-                        >
-                            <Option
-                                image={row.image}
-                                description={row.description}
-                                className={styles.item}
-                            />
-                        </div>
-                        {hoveredIndex === index || actualIndex === index ? (
-                            <div className={styles.dropRight}>ooi</div>
-                        ) : null}
-                    </div>
-                ))}
+  console.log(hoveredIndex);
+  return (
+    <main className={styles.main}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.container}>
+        {rows.map((row, index) => (
+          <div key={index}>
+            <div
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => setActualIndex(index)}
+            >
+              <Option
+                image={row.image}
+                description={row.description}
+                className={styles.item}
+              />
             </div>
-        </main>
-    );
+            {hoveredIndex === index || actualIndex === index ? (
+              <div className={styles.dropRight}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam,
+                rerum unde laborum fugiat esse impedit, autem cum nisi
+                repudiandae cupiditate excepturi quas nostrum officia libero ex
+                deserunt voluptatem voluptates fuga?
+              </div>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
